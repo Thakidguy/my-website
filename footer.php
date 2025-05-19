05aa23fbf6b8f847cbf5cb92324f17e27209ad29
Thakidguy <olawaleshipebi@gmail.com>
2025-05-19 08:13:40 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer>
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> EliteVolt Electricals. All rights reserved.</p>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
