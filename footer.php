76bde5f51383145165fc6422b1773a3ffbd5c75a
Thakidguy <olawaleshipebi@gmail.com>
2025-05-19 08:55:32 +0100

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-  <div class="container">
-    <p>&copy; <?php echo date('Y'); ?> EliteVolt Electricals. All rights reserved.</p>
-  </div>
-</footer>
-<?php wp_footer(); ?>
-</body>
-</html>
